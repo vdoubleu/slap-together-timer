68441abd2c6aefdfd26326f008d3184e4e3e57ce
Victor Wang <45219858+vdoubleu@users.noreply.github.com>
2020-01-08 15:17:41 -0500

    Update timer.py
    changed user input interface to key presses

--- timer.py
+++ timer.py
@@ -1,37 +1,54 @@
 import time
 import json
+import keyboard
 from matplotlib import pyplot as plt
 
 times_file = open(r"times.txt", "r+")
 file_text = times_file.read()
 times = json.loads(file_text)
 
-while True:
-    print("hit enter to start, t for table, e for exit")
-    user_in = input()
+print("press space to start, t for table n to remove lasts one and e/anything else for exit")
 
-    if(user_in == ""):
-        print("start:")
-        curr_time = time.time()
-        
-        if(input() == ""):
+while True:
+    try:
+        if keyboard.is_pressed(' '):
+            print('start:')
+            curr_time = time.time()
+
+            time.sleep(0.25)
+            
+            keyboard.wait(' ')
             print("stop:")
             new_time = time.time()
- 
-        result = new_time - curr_time
-        print("your time:")
-        print(result)
         
-        print("keep this time? type anything for yes, n for no)")
-        if(input() != "n"):
-            times.append(result)
+            result = new_time - curr_time
+            print("your time:")
+            print(result)
+                    
+            times.append(result) 
+            
+            print("press space to start, t for table, n to remove last one and e/anything else for exit")
+            time.sleep(0.25)
 
-    elif(user_in == "t"):
-        plt.plot(times)
-        plt.show()
-    else:
+        elif keyboard.is_pressed('t'):
+            plt.plot(times)
+            plt.show()
+            print("press space to start, t for table, n to remove last one and e/anything else for exit")
+        elif keyboard.is_pressed('n'):
+
+            times.pop()
+            print("press space to start, t for table, n to remove last one and e/anything else for exit")
+            time.sleep(0.25)
+        elif keyboard.is_pressed('e'):
+            
+
+            print("exit")
+            break
+
+    except:
+        print("error")
         break
-    
+
 
 times_file.close()
 
