f6c972ff59a12d0057537e37c3104e51f13d081b
Victor Wang <victorwang2001@gmail.com>
2020-01-10 00:54:01 -0500

    first actual commit

---
+++ t2.py
@@ -0,0 +1,77 @@
+import time
+import json
+import keyboard
+from matplotlib import pyplot as plt
+from random import seed
+from random import choice
+
+def print_scramble(possible_moves):
+    for x in range(0, 15):
+        print(choice(possible_moves), end=" ")
+    print()
+
+def main():
+    possible_moves = ("R", "R'", "R2", "L", "L'", "L2", "U", "U'", "U2", "D", "D'", "D2", "B", "B'", "B2", "F", "F'", "F2")
+
+    seed(1)
+
+    times_file = open(r"times.txt", "r+")
+    file_text = times_file.read()
+    times = json.loads(file_text)
+    
+    print_scramble(possible_moves)
+    print("press space to start, t for table n to remove lasts one and e for exit")
+
+    while True:
+        try:
+            if keyboard.is_pressed(' '):
+                print('start:')
+                curr_time = time.time()
+
+                time.sleep(0.25)
+                
+                keyboard.wait(' ')
+                print("stop:")
+                new_time = time.time()
+            
+                result = new_time - curr_time
+                print("your time:")
+                print(result)
+                        
+                times.append(result) 
+                
+                print_scramble(possible_moves)
+                print("press space to start, t for table, n to remove last one and e for exit")
+                time.sleep(0.25)
+
+            elif keyboard.is_pressed('t'):
+                plt.plot(times)
+                plt.show()
+                print_scramble(possible_moves)
+                print("press space to start, t for table, n to remove last one and e for exit")
+            elif keyboard.is_pressed('n'):
+
+                times.pop()
+                print_scramble(possible_moves)
+                print("press space to start, t for table, n to remove last one and e for exit")
+                time.sleep(0.25)
+            elif keyboard.is_pressed('e'):
+                
+
+                print("exit")
+                break
+
+        except:
+            print("error")
+            break
+
+
+    times_file.close()
+
+    json_string = json.dumps(times)
+    times_file = open(r"times.txt", "w")
+    times_file.write(json_string)
+
+
+if __name__ == "__main__":
+    main()
---
+++ timer.py
@@ -0,0 +1,41 @@
+import time
+import json
+from matplotlib import pyplot as plt
+import keyboard
+
+times_file = open(r"times.txt", "r+")
+file_text = times_file.read()
+times = json.loads(file_text)
+
+while True:
+    print("hit enter to start, t for table, e for exit")
+    user_in = input()
+
+    if(user_in == ""):
+        print("start:")
+        curr_time = time.time()
+        
+        input()
+        print("stop:")
+        new_time = time.time()
+ 
+        result = new_time - curr_time
+        print("your time:")
+        print(result)
+        
+        print("keep this time? type anything for yes, n for no")
+        if(input() != "n"):
+            times.append(result)
+
+    elif(user_in == "t"):
+        plt.plot(times)
+        plt.show()
+    else:
+        break
+    
+
+times_file.close()
+
+json_string = json.dumps(times)
+times_file = open(r"times.txt", "w")
+times_file.write(json_string)
