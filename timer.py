652e1113ec3c0f4afc97b3f4d64fd1dd49ebb963
Victor Wang <victorwang2001@gmail.com>
2020-01-10 16:06:53 -0500

    cleaning up

--- timer.py
+++ timer.py
@@ -2,57 +2,84 @@ import time
 import json
 import keyboard
 from matplotlib import pyplot as plt
+from random import seed
+from random import choice
+import numpy as np
+from numpy.polynomial.polynomial import polyfit
 
-times_file = open(r"times.txt", "r+")
-file_text = times_file.read()
-times = json.loads(file_text)
 
-print("press space to start, t for table n to remove lasts one and e/anything else for exit")
+def print_scramble(possible_moves):
+    for x in range(0, 15):
+        print(choice(possible_moves), end=" ")
+    print()
 
-while True:
-    try:
-        if keyboard.is_pressed(' '):
-            print('start:')
-            curr_time = time.time()
+def main():
+    possible_moves = ("R", "R'", "R2", "L", "L'", "L2", "U", "U'", "U2", "D", "D'", "D2", "B", "B'", "B2", "F", "F'", "F2")
 
-            time.sleep(0.25)
+    seed(1)
+
+    times_file = open(r"times.txt", "r+")
+    file_text = times_file.read()
+    times = json.loads(file_text)
+    
+    print_scramble(possible_moves)
+    print("press space to start, t for table n to remove lasts one and e for exit")
+
+    while True:
+        try:
+            if keyboard.is_pressed(' '):
+                print('start:')
+                curr_time = time.time()
+
+                time.sleep(0.25)
+                
+                keyboard.wait(' ')
+                print("stop:")
+                new_time = time.time()
             
-            keyboard.wait(' ')
-            print("stop:")
-            new_time = time.time()
-        
-            result = new_time - curr_time
-            print("your time:")
-            print(result)
-                    
-            times.append(result) 
-            
-            print("press space to start, t for table, n to remove last one and e/anything else for exit")
-            time.sleep(0.25)
+                result = new_time - curr_time
+                print("your time:")
+                print(result)
+                        
+                times.append(result) 
+                
+                print_scramble(possible_moves)
+                print("press space to start, t for table, n to remove last one and e for exit")
+                time.sleep(0.25)
 
-        elif keyboard.is_pressed('t'):
-            plt.plot(times)
-            plt.show()
-            print("press space to start, t for table, n to remove last one and e/anything else for exit")
-        elif keyboard.is_pressed('n'):
+            elif keyboard.is_pressed('t'):
+                plt.plot(times)
+                x = np.array(list(range(1, len(times))))
+                d, c, b, a = polyfit(x, np.array(times), 3)
+                plt.plot(x, a*(x**3) + b*(x**2) + c*x + d, "-")
+                
+                
+                plt.show()
+                print_scramble(possible_moves)
+                print("press space to start, t for table, n to remove last one and e for exit")
+            elif keyboard.is_pressed('n'):
 
-            times.pop()
-            print("press space to start, t for table, n to remove last one and e/anything else for exit")
-            time.sleep(0.25)
-        elif keyboard.is_pressed('e'):
-            
+                times.pop()
+                print_scramble(possible_moves)
+                print("press space to start, t for table, n to remove last one and e for exit")
+                time.sleep(0.25)
+            elif keyboard.is_pressed('e'):
+                
 
-            print("exit")
+                print("exit")
+                break
+
+        except:
+            print("error")
             break
 
-    except:
-        print("error")
-        break
+
+    times_file.close()
+
+    json_string = json.dumps(times)
+    times_file = open(r"times.txt", "w")
+    times_file.write(json_string)
 
 
-times_file.close()
-
-json_string = json.dumps(times)
-times_file = open(r"times.txt", "w")
-times_file.write(json_string)
-
+if __name__ == "__main__":
+    main()
